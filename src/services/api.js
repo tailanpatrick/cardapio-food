f198b83c9c4950fdb6f33ba3b2d6902600e22526
tailanpatrick <tailanpatrick06@gmail.com>
2024-02-09 12:21:16 -0300

    atualizado endereço server

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL_SERVER = "http://18.231.163.22:3001/";
+const URL_SERVER = "http://18.230.187.156:3001/";
 
 const api = axios.create({
     baseURL: URL_SERVER
