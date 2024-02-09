ab2997c10384352203ff58f32e6af08ee1dd7043
tailanpatrick <tailanpatrick06@gmail.com>
2024-02-09 13:32:05 -0300

    d

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
       name: 'front-cardapio-food',
       script: 'npm',
       args: 'start',
-      cwd: '/home/ubuntu/site-cadapio-food/cardapio-food',
+      cwd: '/home/ubuntu/site-cadapio-food/cardapio-food/src/index.js',
       env: {
         NODE_ENV: 'production'
       }
